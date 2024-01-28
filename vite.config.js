fdc6b9d0add3d243c3680053c4484db426f1387b
ISA OZSOY <samlfstr@gmail.com>
2024-01-28 20:07:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteSvgPlugin from 'vite-plugin-svg';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),ViteSvgPlugin()]
+  plugins: [react()]
 })
